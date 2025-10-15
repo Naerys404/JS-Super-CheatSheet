2de68cdac5b0d372215cf16331a5e0f19531bde2
LauraG <naerys.404@gmail.com>
2025-10-15 14:32:54 +0200

    feat: ajout exemples js dans exercices variables

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
 import './style.css';
 
-let number = 1;
-let string = "Hi there";
-let boolean = true;
-let array = [1,2,3,4,5];
-let object = {name:"John", lastname:"Snow"};
-
-console.log(number);
-console.log(string);
-console.log(boolean);
-console.log(array);
-console.log(object);
-
-console.log(array[2]);
-console.log(object.name);
 
 
